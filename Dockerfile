bbe417325cad77ec216be8bd68c98fd6e4d7c554
Anh Nguyen <ntanh1402@gmail.com>
2018-10-23 11:24:15 +0800

    Add app.js and docker file

---
+++ Dockerfile
@@ -0,0 +1,32 @@
+FROM node:8-slim
+
+RUN apt-get update && \
+apt-get install -yq gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 \
+libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 \
+libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 \
+libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 \
+fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-kacst ttf-freefont \
+ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget --no-install-recommends && \
+apt-get install -yq xvfb && \
+apt-get clean && apt-get autoremove -y && rm -rf /var/lib/apt/lists/* && rm -rf /src/*.deb
+
+# It's a good idea to use dumb-init to help prevent zombie chrome processes.
+ADD https://github.com/Yelp/dumb-init/releases/download/v1.2.0/dumb-init_1.2.0_amd64 /usr/local/bin/dumb-init
+RUN chmod +x /usr/local/bin/dumb-init
+
+# Create app directory
+WORKDIR /root
+
+# Install app dependencies
+# A wildcard is used to ensure both package.json AND package-lock.json are copied
+# where available (npm@5+)
+COPY package.json app.js ./
+
+#RUN npm install
+# If you are building your code for production
+RUN npm install --only=production
+
+EXPOSE 8080
+
+ENTRYPOINT ["dumb-init", "--"]
+CMD [ "xvfb-run", "npm", "start" ]
---
+++ app.js
@@ -0,0 +1,161 @@
+const express = require('express');
+const app = express();
+const {
+  Cluster
+} = require('puppeteer-cluster');
+
+// puppeteer-extra is a drop-in replacement for puppeteer,
+// it augments the installed puppeteer with plugin functionality
+const puppeteer = require('puppeteer-extra')
+
+// register plugins through `.use()`
+puppeteer.use(require('puppeteer-extra-plugin-anonymize-ua')({ makeWindows: true }))
+puppeteer.use(require('puppeteer-extra-plugin-stealth')())
+puppeteer.use(require('puppeteer-extra-plugin-block-resources')({
+  blockedTypes: new Set(['image', 'media', 'stylesheet'])
+})) 
+
+let cluster;
+let getHtml;
+let getUrl;
+let proxy;
+
+async function getCluster(req) {
+  var proxyReq = req.query.proxy;
+  var args = [
+    '--disable-gpu',
+    '--disable-dev-shm-usage',
+    '--disable-setuid-sandbox',
+    '--no-sandbox',
+    '--no-zygote',
+  ];
+  var shouldLaunchCluster = false;
+  if (!cluster) {
+    if (proxyReq) {
+      proxy = proxyReq;
+    }
+    shouldLaunchCluster = true;
+  } else {
+    if (proxyReq && proxy != proxyReq) {
+      await cluster.idle();
+      await cluster.close();
+
+      shouldLaunchCluster = true;
+      proxy = proxyReq;
+    }
+  }
+  if (shouldLaunchCluster) {
+    if (proxy) {
+      args.push('--proxy-server=' + proxy);
+      console.info(`Launch cluster with proxy: ${proxy}`);
+    } else {
+      console.info(`Launch headful cluster.`);
+    }
+
+    cluster = await Cluster.launch({
+        concurrency: Cluster.CONCURRENCY_CONTEXT,
+        maxConcurrency: 4,
+        puppeteer,
+        puppeteerOptions: {
+          headless: false,
+          defaultViewport: {
+            width: 1920,
+            height: 1080,
+          },
+          args: args
+        },
+        retryLimit: 1,
+        timeout: 15000,
+        monitor: false,
+      });
+  }
+  if (!getHtml) {
+    getHtml = async({
+        page,
+        data
+      }) => {
+      const {
+        req,
+        res
+      } = data;
+
+      const url = req.query.url;
+      if (!url) {
+        return res.send('Please provide URL as GET parameter, for example: <a href="?url=https://example.com">?url=https://example.com</a>');
+      }
+      if (proxy) {
+        await page.authenticate({
+          username: 'ntanh1402',
+          password: 'tuananh'
+        });
+      }
+      await page.goto(url, {
+        waitUntil: ['load', 'networkidle2']
+      });
+
+      const htmlStr = await page.content();
+
+      res.set('Content-Type', 'text/html');
+      res.send(htmlStr);
+    };
+  }
+  if (!getUrl) {
+    getUrl = async({
+        page,
+        data
+      }) => {
+      const {
+        req,
+        res
+      } = data;
+
+      const url = req.query.url;
+      if (!url) {
+        return res.send('Please provide URL as GET parameter, for example: <a href="?url=https://example.com">?url=https://example.com</a>');
+      }
+
+      if (proxy) {
+        await page.authenticate({
+          username: 'ntanh1402',
+          password: 'tuananh'
+        });
+      }
+      await page.goto(url, {
+        waitUntil: ['domcontentloaded']
+      });
+
+      const urlStr = await page.url();
+
+      res.set('Content-Type', 'text/html');
+      res.send(urlStr);
+    };
+  }
+}
+
+app.get('/html', function (request, response) {
+  (async() => {
+    await getCluster(request);
+    await cluster.queue({
+      req: request,
+      res: response
+    }, getHtml);
+  })();
+});
+
+app.get('/url', function (request, response) {
+  (async() => {
+    await getCluster(request);
+    await cluster.queue({
+      req: request,
+      res: response
+    }, getUrl);
+  })();
+});
+
+const server = app.listen(8080, err => {
+    if (err) {
+      return console.error(err);
+    }
+    const port = server.address().port;
+    console.info(`App listening on port ${port}`);
+  });
