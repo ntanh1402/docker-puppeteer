27f50942de0da18ad7e6538431cabb01520ce735
Anh Nguyen <ntanh1402@gmail.com>
2019-04-19 14:36:22 +0800

    init

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const { Cluster } = require('puppeteer-cluster');
 
 const puppeteer = require('puppeteer-extra');
 
@@ -12,11 +11,11 @@ puppeteer.use(require('puppeteer-extra-plugin-block-resources')({
 
 (async () => {
 
-  const cluster = await Cluster.launch({
-    concurrency: Cluster.CONCURRENCY_CONTEXT,
-    maxConcurrency: 2,
-    puppeteer,
-    puppeteerOptions: {
+  app.get('/html', async function(req, res) {
+    if (!req.query.url) {
+      return res.end('Please specify url like this: ?url=example.com');
+    }
+    const browser = await puppeteer.launch({
       headless: false,
       defaultViewport: {
         width: 1920,
@@ -29,58 +28,28 @@ puppeteer.use(require('puppeteer-extra-plugin-block-resources')({
         '--no-sandbox',
         '--no-zygote'
       ]
-    },
-    monitor: false,
-  });
+    });
 
-  const getUrl = async ({ page, data: url }) => {
     try {
-      await page.goto(url, {
-        timeout: 20000,
-        waitUntil: ['domcontentloaded']
-      });
+      // Create a new incognito browser context
+      const context = await browser.createIncognitoBrowserContext();
 
-      const urlStr = await page.url();
+      // Create a new page inside context.
+      const page = await context.newPage();
 
-      return urlStr;
-    } catch (err) {
-      throw err;
-    } finally {
-      await page.goto('about:blank');
-      await page.close();
-    }
-  };
-
-  const getHtml = async ({ page, data: url }) => {
-    try {
-      await page.goto(url, {
+      await page.goto(req.query.url, {
         timeout: 20000,
         waitUntil: ['load', 'networkidle2']
       });
-
-      const htmlStr = await page.content();
-
-      return htmlStr;
-    } catch (err) {
-      throw err;
-    } finally {
-      await page.goto('about:blank');
-      await page.close();
-    }
-  };
-
-  app.get('/html', async function(req, res) {
-    if (!req.query.url) {
-      return res.end('Please specify url like this: ?url=example.com');
-    }
-    try {
-      const result = await cluster.execute(req.query.url, getHtml);
+      const result = await page.content();
 
       res.set('Content-Type', 'text/html');
       res.send(result);
     } catch (err) {
       console.error(err);
       res.send(err);
+    } finally {
+      await browser.close();
     }
   });
 
@@ -88,14 +57,42 @@ puppeteer.use(require('puppeteer-extra-plugin-block-resources')({
     if (!req.query.url) {
       return res.end('Please specify url like this: ?url=example.com');
     }
+    const browser = await puppeteer.launch({
+      headless: false,
+      defaultViewport: {
+        width: 1920,
+        height: 1080,
+      },
+      args: [
+        '--disable-gpu',
+        '--disable-dev-shm-usage',
+        '--disable-setuid-sandbox',
+        '--no-sandbox',
+        '--no-zygote'
+      ]
+    });
+
     try {
-      const result = await cluster.execute(req.query.url, getUrl);
+      // Create a new incognito browser context
+      const context = await browser.createIncognitoBrowserContext();
+
+      // Create a new page inside context.
+      const page = await context.newPage();
+
+      await page.goto(req.query.url, {
+        timeout: 20000,
+        waitUntil: ['domcontentloaded']
+      });
+
+      const result = await page.url();
 
       res.set('Content-Type', 'text/plain');
       res.send(result);
     } catch (err) {
       console.error(err);
       res.send(err);
+    } finally {
+      await browser.close();
     }
   });
 
