3046e1daf59d197b69f30f2866d39526bf5e61dd
Anh Nguyen <ntanh1402@gmail.com>
2019-04-19 19:05:54 +0800

    no cluster

--- app.js
+++ app.js
@@ -1,177 +1,145 @@
 const express = require('express');
 const app = express();
-const { Cluster } = require('puppeteer-cluster');
 
-// puppeteer-extra is a drop-in replacement for puppeteer,
-// it augments the installed puppeteer with plugin functionality
 const puppeteer = require('puppeteer-extra');
 
-// register plugins through `.use()`
 puppeteer.use(require('puppeteer-extra-plugin-anonymize-ua')());
 puppeteer.use(require('puppeteer-extra-plugin-stealth')());
 puppeteer.use(require('puppeteer-extra-plugin-block-resources')({
   blockedTypes: new Set(['image', 'media'])
 }));
 
-let cluster;
-let getHtml;
-let getUrl;
-let proxy;
+(async () => {
 
-async function getCluster(req) {
-  var proxyReq = req.query.proxy;
-  var args = [
-    '--disable-gpu',
-    '--disable-dev-shm-usage',
-    '--disable-setuid-sandbox',
-    '--no-sandbox',
-    '--no-zygote',
-  ];
-  var shouldLaunchCluster = false;
-  if (!cluster) {
-    if (proxyReq) {
-      proxy = proxyReq;
-    }
-    shouldLaunchCluster = true;
-  } else {
-    if (proxyReq && proxy != proxyReq) {
-      await cluster.idle();
-      await cluster.close();
-
-      shouldLaunchCluster = true;
-      proxy = proxyReq;
-    }
-  }
-  if (shouldLaunchCluster) {
-    if (proxy) {
-      args.push('--proxy-server=' + proxy);
-      console.info(`Launch cluster with proxy: ${proxy}`);
-    } else {
-      console.info(`Launch headful cluster.`);
+  app.get('/html', async function(req, res) {
+    if (!req.query.url) {
+      return res.end('Please specify url like this: ?url=example.com');
     }
 
-    cluster = await Cluster.launch({
-      concurrency: Cluster.CONCURRENCY_CONTEXT,
-      maxConcurrency: 2,
-      puppeteer,
-      puppeteerOptions: {
+    var args = [
+      '--disable-gpu',
+      '--disable-dev-shm-usage',
+      '--disable-setuid-sandbox',
+      '--no-sandbox',
+      '--no-zygote',
+    ];
+    if (req.query.proxy) {
+      args.push('--proxy-server=' + req.query.proxy);
+    }
+
+    var browser;
+    try {
+      browser = await puppeteer.launch({
         headless: false,
         defaultViewport: {
           width: 1920,
           height: 1080,
         },
         args: args
-      },
-      monitor: false,
-    });
-  }
-  if (!getHtml) {
-    getHtml = async ({ page, data }) => {
-      const { req, res } = data;
+      });
 
-      try {
-        const url = req.query.url;
-        if (!url) {
-          return res.send('Please provide URL as GET parameter, for example: <a href="?url=https://example.com">?url=https://example.com</a>');
-        }
+      // Create a new incognito browser context
+      const context = await browser.createIncognitoBrowserContext();
 
-        if (proxy) {
-          const user = req.query.username;
-          const pass = req.query.password;
+      // Create a new page inside context.
+      const page = await context.newPage();
 
-          await page.authenticate({
-            username: user,
-            password: pass
-          });
-        }
-        await page.goto(url, {
-          timeout: 20000,
-          waitUntil: ['load', 'networkidle2']
+      if (req.query.proxy) {
+        const user = req.query.username;
+        const pass = req.query.password;
+
+        await page.authenticate({
+          username: user,
+          password: pass
         });
-
-        const htmlStr = await page.content();
-
-        res.set('Content-Type', 'text/html');
-        res.send(htmlStr);
-      } catch (error) {
-        console.error(error);
-        res.status(500).send(error);
-      } finally {
-        await page.goto('about:blank');
-        await page.close();
       }
-    };
-  }
-  if (!getUrl) {
-    getUrl = async ({ page, data }) => {
-      const { req, res } = data;
 
-      try {
-        const url = req.query.url;
-        if (!url) {
-          return res.send('Please provide URL as GET parameter, for example: <a href="?url=https://example.com">?url=https://example.com</a>');
-        }
+      await page.goto(req.query.url, {
+        timeout: 20000,
+        waitUntil: ['load', 'networkidle2']
+      });
 
-        if (proxy) {
-          const user = req.query.username;
-          const pass = req.query.password;
+      const result = await page.content();
 
-          await page.authenticate({
-            username: user,
-            password: pass
-          });
-        }
-        await page.goto(url, {
-          timeout: 20000,
-          waitUntil: ['domcontentloaded']
+      res.set('Content-Type', 'text/html');
+      res.send(result);
+    } catch (err) {
+      console.error(err);
+      res.send(err);
+    } finally {
+      if (browser) {
+        await browser.close();
+      }
+    }
+  });
+
+  app.get('/url', async function(req, res) {
+    if (!req.query.url) {
+      return res.end('Please specify url like this: ?url=example.com');
+    }
+
+    var args = [
+      '--disable-gpu',
+      '--disable-dev-shm-usage',
+      '--disable-setuid-sandbox',
+      '--no-sandbox',
+      '--no-zygote',
+    ];
+    if (req.query.proxy) {
+      args.push('--proxy-server=' + req.query.proxy);
+    }
+    var browser;
+    try {
+      browser = await puppeteer.launch({
+        headless: false,
+        defaultViewport: {
+          width: 1920,
+          height: 1080,
+        },
+        args: args
+      });
+
+      // Create a new incognito browser context
+      const context = await browser.createIncognitoBrowserContext();
+
+      // Create a new page inside context.
+      const page = await context.newPage();
+
+      if (req.query.proxy) {
+        const user = req.query.username;
+        const pass = req.query.password;
+
+        await page.authenticate({
+          username: user,
+          password: pass
         });
-
-        const urlStr = await page.url();
-        await page.goto('about:blank');
-        await page.close();
-
-        res.set('Content-Type', 'text/plain');
-        res.send(urlStr);
-      } catch (error) {
-        console.error(error);
-        res.status(500).send(error);
-      } finally {
-        await page.goto('about:blank');
-        await page.close();
       }
-    };
-  }
-}
 
-app.get('/html', function(request, response) {
-  (async () => {
-    await getCluster(request);
-    await cluster.queue({
-      req: request,
-      res: response
-    }, getHtml);
-  })();
-});
+      await page.goto(req.query.url, {
+        timeout: 20000,
+        waitUntil: ['domcontentloaded']
+      });
 
-app.get('/url', function(request, response) {
-  (async () => {
-    await getCluster(request);
-    await cluster.queue({
-      req: request,
-      res: response
-    }, getUrl);
-  })();
-});
+      const result = await page.url();
 
-app.get('/healthcheck', function(request, response) {
-  (async () => {
-    await response.send('OK');
-  })();
-});
+      res.set('Content-Type', 'text/plain');
+      res.send(result);
+    } catch (err) {
+      console.error(err);
+      res.send(err);
+    } finally {
+      if (browser) {
+        await browser.close();
+      }
+    }
+  });
 
-const server = app.listen(8080, err => {
-  if (err) {
-    return console.error(err);
-  }
-  console.info('App listening on port 8080');
-});
+  app.get('/healthcheck', async function(req, res) {
+    res.send('OK');
+  });
+
+  app.listen(8080, function() {
+    console.log('Server listening on port 8080.');
+  });
+
+})();
